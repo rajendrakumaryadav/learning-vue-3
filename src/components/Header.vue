5ea9ef8690811c3b3de514c97cfdae7489992d6c
Rajendra Kumar R Yadav <13816347+rajendrakumaryadav@users.noreply.github.com>
2021-09-19 15:59:24 +0530

    Fetched covid data using Browser's fetch api. and logging it to the console.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,28 @@
 <template>
   <header class="text-center bg-yellow-500 text-white p-4 mb-10">
-    <div class="text-3xl md:text-5xl font-bold"> <i class="fa fa-viruses"></i> Covid - 19 Tracker</div>
+    <div class="text-3xl md:text-5xl font-bold"><i class="fa fa-viruses"></i> Covid - 19 Tracker</div>
   </header>
 </template>
 
 <script>
 export default {
-  name: 'Header'
+  name: 'Header',
+  components: {},
+  data () {
+    return {
+      loading: true
+    }
+  },
+  methods: {
+    async fetchCovidData () {
+      const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data
+    }
+  },
+  async created () {
+    const data = await this.fetchCovidData()
+    console.log(data)
+  }
 }
 </script>
